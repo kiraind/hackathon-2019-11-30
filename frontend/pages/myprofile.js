4a33931fb9cece86611319d1288e17567eceab90
NikSh99 <shoshin.nikita@mail.ru>
2019-11-30 16:06:18 +0300

    Update myprofile.js

--- frontend/pages/myprofile.js
+++ frontend/pages/myprofile.js
@@ -12,7 +12,13 @@ const MyProfile = () => {
     }
 
     return (
-        <Wrapper>MyProfile</Wrapper>
+        <Wrapper>Имя: Василий</Wrapper>
+        <Wrapper>Фамилия: Васильев</Wrapper>
+        <Wrapper>Отчество: Васильевич</Wrapper>
+        <Wrapper>E-mail: VasilExample@mail.ru</Wrapper>
+        <Wrapper>Город: Калуга</Wrapper>
+        <Wrapper>Телефон: 8(910)8008080</Wrapper>
+        <Wrapper>Дата регистрации: 30.11.2019</Wrapper>
     )    
 }
 
