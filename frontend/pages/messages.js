79bde2679313075d11219b552d6dbada79a72255
NikSh99 <shoshin.nikita@mail.ru>
2019-11-30 16:28:20 +0300

    Update messages.js

--- frontend/pages/messages.js
+++ frontend/pages/messages.js
@@ -1,18 +1,52 @@
 import Wrapper from '../components/Wrapper.js'
+const Contacts = () => {
+    const contacts = [
+        {
+            id: 1,
+            name: 'Иван Иванов',
+        },
+        },
+    ]
 
-const Messages = () => (
-    <Wrapper>Messages</Wrapper>
+    return (
         <Wrapper>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Иван Иванов:</td>
-                        <td>Вы были очевидцем дтп на Московской вчера вечером?</td>
-                    </tr>                    
-                </tbody>  
-            </table>
+            {contacts.map(user => (
+                <div
+                    key={user.id}
+                    className="ContactItem"
+                >
+                    <div className="ContactAvatar"></div>
+                    <div
+                         className="ContactName"
+                    >{user.name}</div>
+                    <td>Вы были очевидцем дтп на Московской вчера вечером?</td>
+                </div>
+            ))}
 
+            <style>{`
+                .ContactItem {
+                    display: flex;
+                    height: 55px;
+                    font-size: 18px;
+                    font-family: Roboto;
+                    align-items: center;
+                }
+
+                .ContactAvatar {
+                    height: 35px;
+                    width: 35px;
+                    background-color: #eaeaea;
+                    border-radius: 50%;
+                    margin: 0 10px;
+                }
+
+                .ContactName {
+                    
+                }
+            `}</style>
         </Wrapper>
-)
+    )
+}
+
 
 export default Messages
