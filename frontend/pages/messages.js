7fed21fd7e4a5be2d88a799f3edc19b5cec9e259
NikSh99 <shoshin.nikita@mail.ru>
2019-11-30 16:25:34 +0300

    Update messages.js

--- frontend/pages/messages.js
+++ frontend/pages/messages.js
@@ -2,6 +2,17 @@ import Wrapper from '../components/Wrapper.js'
 
 const Messages = () => (
     <Wrapper>Messages</Wrapper>
+        <Wrapper>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Иван Иванов:</td>
+                        <td>Вы были очевидцем дтп на Московской вчера вечером?</td>
+                    </tr>                    
+                </tbody>  
+            </table>
+
+        </Wrapper>
 )
 
 export default Messages
